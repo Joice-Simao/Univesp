337744542d5622b2947f0d0152d869d3414d9dfd
Joice-Simao <joice.simao@hotmail.com>
2025-03-08 13:19:26 -0300

    Add Exercício 2

---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+"""Exercício 2.6
+Primeiro, execute a atribuição palavras =
+['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+Agora, escreva duas expressões Python que são avaliadas, respectivamente,
+como a primeira e a última palavras em palavras, na ordem do dicionário."""
+
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+
+print (min(palavras))
+print (max(palavras))
