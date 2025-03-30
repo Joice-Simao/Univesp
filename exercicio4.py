fd1740fe88bb114cc0668df523f244ca39cbfab6
Joice-Simao <joice.simao@hotmail.com>
2025-03-30 20:54:23 -0300

    Add Exercício 4

---
+++ exercicio4.py
@@ -0,0 +1,15 @@
+"""
+Exercício 3. Cap. 5 – Ex 1c
+Desenvolver os diagramas de blocos e as codificações em português estruturado
+usando laço incondicional (para) do seguinte problema: Construir um programa
+que apresente a soma dos cem primeiros números
+naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+"""
+
+Início
+    soma ← 0
+    Para i de 1 até 100 faça
+        soma ← soma + i
+    FimPara
+    Escreva "A soma dos 100 primeiros números naturais é: ", soma
+Fim
